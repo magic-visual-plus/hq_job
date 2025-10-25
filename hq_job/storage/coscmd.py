6b1d1ff7b8c0dadd450411ef6a235c657b5b74e1
zouxiaochuan <zouxiaochuan@163.com>
2025-10-26 01:35:13 +0800

    add retry for coscmd upload

--- hq_job/storage/coscmd.py
+++ hq_job/storage/coscmd.py
@@ -36,7 +36,12 @@ class COSCMDStorage(StorageBase):
         if os.path.isdir(local_path):
             recursive = "-r"
             pass
-        retcd = os.system(f"coscmd upload {recursive} {local_path} {remote_path}")
+
+        for itry in range(self.retry):
+            retcd = os.system(f"coscmd upload {recursive} {local_path} {remote_path}")
+            if retcd == 0:
+                break
+            pass
 
         if retcd != 0:
             raise RuntimeError(f"coscmd upload {local_path} {remote_path} failed: {retcd}")
